f35c4d7a4a288dfe0e4bfb7c09b6b686b5278855
Patrick Steiger <psteiger@motorola.com>
2019-10-09 18:14:56 -0300

    v1

--- app/src/main/java/com/freelapp/myapplication/UsersViewModel.kt
+++ app/src/main/java/com/freelapp/myapplication/UsersViewModel.kt
@@ -1,7 +1,13 @@
 package com.freelapp.myapplication
 
 import androidx.lifecycle.ViewModel
+import com.google.firebase.database.GenericTypeIndicator
+import java.util.HashMap;
 
 class UsersViewModel : ViewModel() {
-    val usersLiveData = FirebaseResourceLiveData<HashMap<String, User>>("users")
+    val usersLiveData =
+        ResourceRepositoryImpl(
+            "users",
+            object : GenericTypeIndicator<HashMap<String, User>>() {}
+        ).allUsers
 }
